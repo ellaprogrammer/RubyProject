ff73f606a24569d8947300fc4b63c1832b26bb13
ellaprogrammer <ella@diamondsis.com>
2018-02-05 17:49:02 -0800

    Create new Pics actions and view files

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,4 +1,20 @@
 class PicsController < ApplicationController
 	def index
+		#will correspond to the index.html.haml file
+	end
+
+	def new
+		#will correspond to the new.html.haml file
+		@pic = Pic.new #creates an instance of a Pic
+	end
+
+	def create
+		@pic = Pic.new(pic_params)
+	end
+
+	private #so it applies to all the other actions instead of repeating yourself
+
+	def pic_params
+		params.require(:pic).permit(:title, :description)
 	end
 end
